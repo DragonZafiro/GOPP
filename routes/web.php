0e98592c8c3311a44e77eee8445151b298e93863
DragonZafiro <dragonzafiro666@gmail.com>
2018-10-17 21:08:23 -0500

    Cambios web.php
    Prueba2

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/about', 'WebController@about')->name('about');
 // Route::get('/test', function(){
 //     return view('mapa');
 // });
-// Logeo
+// Login prueba 2
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
